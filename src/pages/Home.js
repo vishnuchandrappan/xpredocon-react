8fbd1d2a46c8604b877b1d54f665aefbcfec12fd
vishnuchandrappan <cvishnu70@gmail.com>
2020-02-12 22:25:45 +0530

    basic layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
-    return (
-        <div>
-            HOME
+  return (
+    <React.Fragment>
+      <div className="main">
+        <div className="content">
+          <input type="text" />
+          <div className="buttons">
+            <button>
+              <i className="fa fa-microphone"></i>
+            </button>
+            <button>
+              <i className="fa fa-search"></i>
+            </button>
+          </div>
         </div>
-    )
+      </div>
+      <div className="bg"></div>
+    </React.Fragment>
+  );
 }
